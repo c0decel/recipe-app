0311e7c7a155ab395b4603c8407ab35e67038909
c0decel <124975714+c0decel@users.noreply.github.com>
2024-02-21 17:46:29 -0800

    Add files via upload

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -6,6 +6,7 @@ class Recipe(models.Model):
     cook_time = models.FloatField(help_text='in minutes')
     ingredients = models.CharField(max_length=120)
     description = models.TextField(max_length=200)
+    steps = models.TextField(max_length=1000, help_text='Seperate each step with two commas')
     image = models.ImageField(upload_to='recipe_images', default='no_img.PNG')
 
     def calc_difficulty(self):
@@ -27,6 +28,13 @@ class Recipe(models.Model):
         ingredients_list = self.ingredients.split(',')
         return ingredients_list
 
+    def steps_to_list(self):
+        steps_list = self.steps.split(',, ')
+        numbered_steps = []
+        for index, item in enumerate(steps_list):
+            numbered_item = f'{index + 1}. {item}'
+            numbered_steps.append(numbered_item)
+        return numbered_steps
 
     def get_absolute_url(self):
         return reverse('recipes:details', kwargs={'pk': self.pk})
--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -4,7 +4,7 @@ from .models import Recipe
 class RecipeModelTest(TestCase):
 
     def setUpTestData():
-        Recipe.objects.create(name='Tea', cook_time=5, ingredients='tea leaves, water, sugar', description='Steep tea leaves in boiling water and add sugar.')
+        Recipe.objects.create(name='Tea', cook_time=5, ingredients='tea leaves, water, sugar', description='Steep tea leaves in boiling water and add sugar.', steps='Boil water,, Add tea leaves,, Add sugar')
 
     def test_name(self):
         recipe = Recipe.objects.get(id=1)
@@ -19,3 +19,13 @@ class RecipeModelTest(TestCase):
     def test_calc_difficulty(self):
         recipe = Recipe.objects.get(id=1)
         self.assertEqual(recipe.calc_difficulty(), 'Easy')
+
+    def test_steps(self):
+        recipe = Recipe.objects.get(id=1)
+        recipe_steps = recipe._meta.get_field('steps').help_text
+        self.assertEqual(recipe_steps, 'Seperate each step with two commas')
+
+    def test_steps_to_list(self):
+        recipe = Recipe.objects.get(id=1)
+        expected_steps = ['1. Boil water', '2. Add tea leaves', '3. Add sugar']
+        self.assertEqual(recipe.steps_to_list(), expected_steps)
